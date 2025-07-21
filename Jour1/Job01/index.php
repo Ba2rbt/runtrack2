626157143395a0ab0e1b5e87a135ace4dc8c0440
Ba2 <babasvn19@hotmail.com>
2025-07-21 15:00:23 +0200

    Add files via upload

---
+++ Jour1/Job01/index.php
@@ -0,0 +1,11 @@
+<?php
+$str = "LaPlateforme";
+$str2 = "Vive";
+$str3 = "!";
+echo $str2; echo $str; echo $str3;
+$val = "6";
+$val += "4";
+echo $val;
+$mybool = false;
+echo $mybool ? "true" : "false";
+
