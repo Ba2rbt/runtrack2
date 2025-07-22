3934f55bb908e2af05c4c4995c55b776d3f282ba
Ba2 <babasvn19@hotmail.com>
2025-07-22 15:22:14 +0200

    Delete Jour2/index.php

--- Jour2/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-for ($i = 0; $i <= 1337; $i++) {
-    if ($i == 26 || $i == 37 || $i == 88 || $i == 1111 || $i == 3233) {
-        continue; // Ces nombres seront masqués (non affichés)
-    }
-    echo $i . "<br>";
-}
-?>
