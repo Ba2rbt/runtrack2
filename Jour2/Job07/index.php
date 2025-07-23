90f70e29023415373ea6142c7686f5d4005cf01d
Ba2 <babasvn19@hotmail.com>
2025-07-23 16:56:32 +0200

    Add files via upload

--- Jour2/Job07/index.php
+++ Jour2/Job07/index.php
@@ -4,8 +4,13 @@ $hauteur = 5;
 
 // Boucle pour générer chaque ligne du triangle
 for ($i = 1; $i <= $hauteur; $i++) {
-    // Affiche les étoiles selon le numéro de ligne actuelle
-    for ($j = 1; $j <= $i; $j++) {
+    // Afficher les espaces pour centrer le triangle
+    for ($j = 1; $j <= ($hauteur - $i); $j++) {
+        echo "&nbsp;"; // Utilise &nbsp; pour l'affichage HTML
+    }
+    
+    // Affiche les étoiles pour former un triangle complet
+    for ($k = 1; $k <= (2 * $i - 1); $k++) {
         echo "*";
     }
     // Aller à la ligne suivante
